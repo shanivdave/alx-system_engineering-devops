f589191a152995700646ff177d206dc9d7cee2f4
shanivdave <32578797+shanivdave@users.noreply.github.com>
2022-09-16 21:15:43 +0300

    confi

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# install puppet-lint -v 2.5.0
-
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
-}
+# Installing a package with a particular version
+package { 'puppet-lint':
+  ensure   => '2.5.0',
+  provider => gem;
+  }
